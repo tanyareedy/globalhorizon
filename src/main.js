2a48e42142951fa92c97b58c9ae576fedb630a42
Katalyst33 <katalyst33>
2019-07-09 01:16:53 +0100

    Vue mpa

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
+import 'bootstrap'
 import Vue from 'vue'
 import App from './App.vue'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+
+
+// Require the main Sass manifest file
+require('./assets/scss/main.scss');
+
+
+Vue.config.productionTip = false;
+
+Vue.component('Nav', Nav);
+Vue.component('Footer', Footer);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+
